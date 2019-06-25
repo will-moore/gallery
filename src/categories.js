981e75b4d6546285315283a3f15228ff41601bcd
William Moore <w.moore@dundee.ac.uk>
2019-06-25 11:16:49 +0100

    Use gallery_settings context for categories

--- src/categories.js
+++ src/categories.js
@@ -1,35 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Category from './Category';
 import { filterStudiesByMapQuery } from './model/filterStudies';
+import SettingsContext from './model/context';
 
 function Categories({studies, superCategory}) {
 
-  const superCategories = {
-    'cell': {'query': 'Sample Type:cell',
-            'label': 'Cell - IDR',
-            'title': 'Welcome to Cell-IDR'},
-    'tissue': {'query': 'Sample Type:tissue',
-            'label': 'Tissue - IDR',
-            'title': 'Welcome to Tissue-IDR'},
-  }
+  const gallerySettings = useContext(SettingsContext)
 
-  const categories = [
-    {"label": "Most Recent", "index": 0, "query": "LAST10:date"},
-    {"label": "Time-lapse imaging", "index": 1, "query": "Study Type:time OR Study Type:5D OR Study Type:3D-tracking"},
-    {"label": "Light sheet fluorescence microscopy", "index": 2, "query": "Study Type:light sheet"},
-    {"label": "Protein localization studies", "index": 3, "query": "Study Type:protein localization"},
-    {"label": "Digital pathology imaging", "index": 4, "query":"Study Type:histology"},
-    {"label": "Yeast studies", "index": 5, "query": "Organism: Saccharomyces cerevisiae OR Organism:Schizosaccharomyces pombe"},
-    {"label": "High-content screening (human)", "index": 6, "query": "Organism:Homo sapiens AND Study Type:high content screen"}
-  ];
+  let categories = [];
+  // If not configured, won't see any list of categories.
+  if (gallerySettings.CATEGORY_QUERIES) {
+    for (let category in gallerySettings.CATEGORY_QUERIES) {
+      categories.push({...gallerySettings.CATEGORY_QUERIES[category], id:category})
+    }
+  }
 
   // If we got a 'superCategory' from URL, check it is valid...
   if (superCategory !== undefined) {
+    const superCategories = gallerySettings.SUPER_CATEGORIES;
     if (superCategories[superCategory]) {
       let query = superCategories[superCategory].query;
       studies = filterStudiesByMapQuery(studies, query);
     } else {
-      // superCategory not supported ~ 404
+      // superCategory not found ~ 404
       return <h2>Category '{superCategory}' not found</h2>
     }
   }
