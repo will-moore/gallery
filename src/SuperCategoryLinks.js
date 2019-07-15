666f1b29f0c7cd90599465944201905f8223f04d
William Moore <w.moore@dundee.ac.uk>
2019-07-15 13:32:36 +0100

    SuperCategoryLinks.js uses config instead of hard-coding

--- src/SuperCategoryLinks.js
+++ src/SuperCategoryLinks.js
@@ -1,33 +1,44 @@
 
 import React from 'react';
+import { AbsoluteLink as Link } from "./router/wrappers";
 
 export default ({superCategory, superCategories}) => {
 
   // Hide image links if we have a superCategory
   let scStyle = {'display': superCategory ? 'none': 'block'};
 
+  superCategories = superCategories || {};
+
+  let links = [];
+  
+  for (let category in superCategories) {
+    links.push({
+      ...superCategories[category],
+      id: category
+    });
+  }
+
   return (
     <div className="row horizontal super_categories"
       style={scStyle}>
       <div className="small-0 medium-1 large-2 columns">&nbsp;</div>
-      
-      <a href="cell/" className="scLink">
-        <div className="small-12 medium-5 large-4 columns">
-          <img className="thumbnail" src="https://idr.openmicroscopy.org/webgateway/render_image/122770/0/0/" />
-          <div className="img_overlay">
-            <span className="overlay_text">Cell - IDR</span>
-          </div>
-        </div>
-      </a>
-  
-      <a href="tissue/" className="scLink">
-        <div className="small-12 medium-5 large-4 columns">
-          <img className="thumbnail" src="https://idr.openmicroscopy.org/webgateway/render_image_region/5470164/0/0/?region=1024,1024,696,520" />
-          <div className="img_overlay">
-            <span className="overlay_text">Tissue - IDR</span>
-          </div>
-        </div>
-      </a>
+      {
+        links.map(category => (
+          <Link
+            to={`/${category.id}/`}
+            key={category.id}
+            className="scLink">
+            <div className="small-12 medium-5 large-4 columns">
+              <img
+                alt={`Example from ${ category.label }`}
+                className="thumbnail" src={category.image} />
+              <div className="img_overlay">
+                <span className="overlay_text">{category.label}</span>
+              </div>
+            </div>
+          </Link>
+        ))
+      }
   
       <div className="small-2 medium-2 large-2 columns"></div>  
     </div>
